c01b24ca1fcf39246bb95eac3461ef98969bad93
Cheersupzoo <cheersupzoo@gmail.com>
2022-01-30 19:07:23 +0700

    add roadmap responsive

--- components/road_map/index.tsx
+++ components/road_map/index.tsx
@@ -31,14 +31,14 @@ export default function RoadMap() {
             <div className="absolute top-1/2 left-0 right-0 -z-10 -translate-y-1/2">
               <Image className="" src={HeaderBox} alt="HeaderBox" />
             </div>
-            <div className=" mx-14">
-              <div className="text-gold-gradient font-tavi text-5xl font-medium ">
+            <div className=" mx-8 md:mx-14">
+              <div className="text-gold-gradient font-tavi text-3xl md:text-5xl font-medium ">
                 ROADMAP
               </div>
             </div>
           </div>
 
-          <div className="relative mt-16 flex flex-col items-center gap-y-80 pb-8">
+          <div className="relative mt-8 md:mt-16 flex flex-col items-center gap-y-10 md:gap-y-80 pb-8 -translate-x-20 md:translate-x-0">
             <div className="absolute inset-0 flex flex-col">
               {/* <div className="w-1 h-full mx-auto border-l-2 border-white border-dashed"></div> */}
               <div className="relative h-full  overflow-hidden mt-24 mb-32">
@@ -60,10 +60,10 @@ export default function RoadMap() {
             </div>
             <div className="absolute inset-0 flex flex-col">
               {/* <div className="w-1 h-full mx-auto border-l-2 border-white border-dashed"></div> */}
-              <div className="relative h-[45rem] -mt-8 ">
+              <div className="relative h-[11rem] md:h-[45rem] md:-mt-8 ">
                 {/* <div className="w-4 h-32 bg-gold-gradient-strong mx-auto rounded-[50%] rounded-tr-none -rotate-45" /> */}
                 <Image
-                  className="absolute left-0 right-0 droplet w-10 mx-auto"
+                  className="absolute left-0 right-0 droplet w-7 md:w-10 mx-auto"
                   src={droplet}
                   alt="droplet"
                 />
@@ -71,139 +71,124 @@ export default function RoadMap() {
             </div>
             <div className="relative">
               <Image
-                className="w-52"
+                className="w-36 md:w-52"
                 src={m1}
                 alt="milestone1"
                 priority={true}
               />
-              <div className="absolute flex flex-col justify-center top-0 bottom-0 right-0 translate-x-full">
-                <div className="font-source font-semibold text-lg text-gold-gradient">
-                  MILESTONE 1
-                </div>
-                <div className="font-source font-semibold text-lg text-white">
-                  Q1 / 2022
-                </div>
-                <div className="font-source text-xs text-white uppercase">
-                  game trailer
-                  <br /> white paper
-                  <br /> communities launching
-                  <br /> seed & Private Sale
-                </div>
-              </div>
+              <MilestoneLeft title="MILESTONE 1" subtitle="Q1 / 2022">
+                game trailer
+                <br /> white paper
+                <br /> communities launching
+                <br /> seed & Private Sale
+              </MilestoneLeft>
             </div>
 
             <div className="relative">
               <Image
-                className="w-52 p-4"
+                className="w-36 md:w-52 p-4"
                 src={m2}
                 alt="milestone2"
                 priority={true}
               />
-              <div className="absolute flex flex-col justify-center items-end top-0 bottom-0 left-0 -translate-x-full">
-                <div className="font-source font-semibold text-lg text-gold-gradient">
-                  MILESTONE 2
-                </div>
-                <div className="font-source font-semibold text-lg text-white">
-                  Q2 / 2022
-                </div>
-                <div className="font-source text-xs text-white uppercase text-right">
-                  public sale & TGE
-                  <br />
-                  Releasing NFT’s Characters
-                </div>
-              </div>
+              <MilestoneRight title="MILESTONE 2" subtitle="Q2 / 2022">
+                public sale & TGE
+                <br />
+                Releasing NFT’s Characters
+              </MilestoneRight>
             </div>
 
             <div className="relative">
               <Image
-                className="w-52 p-4"
+                className="w-36 md:w-52 p-4"
                 src={m3}
                 alt="milestone3"
                 priority={true}
               />
-              <div className="absolute flex flex-col justify-center top-0 bottom-0 right-0 translate-x-full">
-                <div className="font-source font-semibold text-lg text-gold-gradient">
-                  MILESTONE 3
-                </div>
-                <div className="font-source font-semibold text-lg text-white">
-                  Q3 / 2022
-                </div>
-                <div className="font-source text-xs text-white uppercase">
-                  NFT PRE-SALE
-                  <br />
-                  $EVM Staking
-                  <br />
-                  NFT MarketPLACE
-                </div>
-              </div>
+              <MilestoneLeft title="MILESTONE 3" subtitle="Q3 / 2022">
+                NFT PRE-SALE
+                <br />
+                $EVM Staking
+                <br />
+                NFT MarketPLACE
+              </MilestoneLeft>
             </div>
 
             <div className="relative">
               <Image
-                className="w-52 p-4"
+                className="w-36 md:w-52 p-4"
                 src={m4}
                 alt="milestone4"
                 priority={true}
               />
-              <div className="absolute flex flex-col justify-center items-end top-0 bottom-0 left-0 -translate-x-full">
-                <div className="font-source font-semibold text-lg text-gold-gradient">
-                  MILESTONE 4
-                </div>
-                <div className="font-source font-semibold text-lg text-white">
-                  Q4 / 2022
-                </div>
-                <div className="font-source text-xs text-white uppercase text-right">
-                  building chalenges
-                </div>
-              </div>
+              <MilestoneRight title="MILESTONE 4" subtitle="Q4 / 2022">
+                building chalenges
+              </MilestoneRight>
             </div>
 
             <div className="relative">
               <Image
-                className="w-52 p-4"
+                className="w-36 md:w-52 p-4"
                 src={m5}
                 alt="milestone5"
                 priority={true}
               />
-              <div className="absolute flex flex-col justify-center top-0 bottom-0 right-0 translate-x-full">
-                <div className="font-source font-semibold text-lg text-gold-gradient">
-                  MILESTONE 5
-                </div>
-                <div className="font-source font-semibold text-lg text-white">
-                  Q1 / 2023
-                </div>
-                <div className="font-source text-xs text-white uppercase">
-                  beta game launch
-                  <br />
-                  OFFICIAL LAUNCH
-                </div>
-              </div>
+              <MilestoneLeft title="MILESTONE 5" subtitle="Q1 / 2023">
+                beta game launch
+                <br />
+                OFFICIAL LAUNCH
+              </MilestoneLeft>
             </div>
 
             <div className="relative">
               <Image
-                className="w-52 p-4"
+                className="w-36 md:w-52 p-4"
                 src={m6}
                 alt="milestone6"
                 priority={true}
               />
-              <div className="absolute flex flex-col justify-center items-end top-0 bottom-0 left-0 -translate-x-full">
-                <div className="font-source font-semibold text-lg text-gold-gradient">
-                  MILESTONE 6
-                </div>
-                <div className="font-source font-semibold text-lg text-white">
-                  Q2 / 2023
-                </div>
-                <div className="font-source text-xs text-white uppercase text-right">
-                  E-Sport Tournament
-                  <br />
-                  Ranking rewards
-                </div>
-              </div>
+              <MilestoneRight title="MILESTONE 6" subtitle="Q2 / 2023">
+                E-Sport Tournament
+                <br />
+                Ranking rewards
+              </MilestoneRight>
             </div>
           </div>
         </div>
       </div>
     </section>
+  );
+}
+
+type MilestoneProps = { title: string; subtitle: string; children: any };
+function MilestoneLeft({ title, subtitle, children }: MilestoneProps) {
+  return (
+    <div className="absolute  flex flex-col justify-center top-0 bottom-0 -right-7 md:right-0 translate-x-full">
+      <div className="font-source font-semibold text-sm md:text-lg text-gold-gradient">
+        {title}
+      </div>
+      <div className="font-source font-semibold text-sm md:text-lg text-white">
+        {subtitle}
+      </div>
+      <div className="mt-2 font-source text-xs text-white uppercase">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+function MilestoneRight({ title, subtitle, children }: MilestoneProps) {
+  return (
+    <div className="absolute flex flex-col justify-center md:items-end top-0 bottom-0 -right-7 md:right-auto md:left-0 translate-x-full md:-translate-x-full">
+      <div className="font-source font-semibold text-sm md:text-lg text-gold-gradient">
+        {title}
+      </div>
+      <div className="font-source font-semibold text-sm md:text-lg text-white">
+        {subtitle}
+      </div>
+      <div className="mt-2 font-source text-xs text-white uppercase md:text-right">
+        {children}
+      </div>
+    </div>
   );
 }
