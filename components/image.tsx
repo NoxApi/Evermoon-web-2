a834a25b40ce1a47e47985da1015a2972f164d9d
Cheersupzoo <cheersupzoo@gmail.com>
2022-02-03 22:39:20 +0700

    add image blur placeholder for png

--- components/image.tsx
+++ components/image.tsx
@@ -7,6 +7,23 @@ interface ImageProps {
   priority?: boolean
 }
 function Image({ className, src, alt, priority }: ImageProps) {
+  if(src.src.includes('png')) {
+    return (
+      <div className={className}>
+        <div className={'relative'}>
+          <ImageNext
+            layout="responsive"
+            objectFit="contain"
+            src={src}
+            alt={alt}
+            priority={priority}
+            placeholder='blur'
+          />
+        </div>
+      </div>
+    )
+  }
+  
   return (
     <div className={className}>
       <div className={'relative'}>
