bb79a38b42d58a77a81477a1eed56a837a879f01
cheersupzoo <cheersupzoo@gmail.com>
2022-04-18 00:32:06 +0700

    fix import error

--- components/roadmap/index.tsx
+++ components/roadmap/index.tsx
@@ -6,7 +6,7 @@ import q422 from '../../assets/roadmap/22q4.svg'
 import q123 from '../../assets/roadmap/23q1.svg'
 import q223 from '../../assets/roadmap/23q2.svg'
 import Image from '../image'
-import styled, { keyframes } from 'styled-components'
+import styled, { jsx, css, keyframes } from '@emotion/react'
 import BG from '../../assets/roadmap/bg.png'
 
 const dotProgress = (index: number) => keyframes`
@@ -18,19 +18,10 @@ const dotProgress = (index: number) => keyframes`
   }
 `
 
-const Dot = styled.div`
-  animation: 1.5s infinite
-    ${(props) => dotProgress((props as any).index as number)};
-  animation-timing-function: step-end;
-  width: 8px;
-  height: 8px;
-  border-radius: 9999px;
-` as ({ index }: { index: number }) => JSX.Element
-
 export const Roadmap = () => {
   return (
     <section className="relative z-0">
-			<Image src={BG} alt="BG" className=" absolute inset-0 -z-10 mt-32"  />
+      <Image src={BG} alt="BG" className=" absolute inset-0 -z-10 mt-32" />
       <div className="mt-20 max-w-[1024px] mx-auto font-Josefin text-gold-light text-6xl text-center relative">
         ROADMAP
       </div>
@@ -53,7 +44,15 @@ export const Roadmap = () => {
             {Array(9)
               .fill(0)
               .map((e, index) => (
-                <Dot key={index} index={index}></Dot>
+                // <Dot key={index} index={index}></Dot>
+                <div
+                  key={index}
+                  className="w-2 h-2 rounded-full"
+                  css={css`
+                    animation: 1.5s infinite ${dotProgress(index)};
+                    animation-timing-function: step-end;
+                  `}
+                />
               ))}
           </div>
           <div>
