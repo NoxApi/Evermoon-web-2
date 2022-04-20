72c90826946830ead442f1a96e476054871c3ea0
cheersupzoo <cheersupzoo@gmail.com>
2022-04-20 09:03:19 +0700

    add CTA

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -16,6 +16,9 @@ import ImageNext from 'next/image'
 import JoinProgram from '../../assets/home/joinprogram.svg'
 import JoinSale from '../../assets/home/joinsale.svg'
 import Whitepaper from '../../assets/home/whitepaper.svg'
+import CTA from '../../assets/socials/CTA.svg'
+import frameCTA from '../../assets/socials/frameCTA.svg'
+import { useState } from 'react'
 
 export default function Home() {
   return (
@@ -77,6 +80,42 @@ export default function Home() {
           </LinkNewTab>
         </div>
       </div>
+
+      <SocialCTA />
     </section>
+  )
+}
+
+function SocialCTA() {
+  return (
+    <div className='group '>
+      <Image
+        src={CTA}
+        alt="cta"
+        className="absolute top-1/2 left-0 w-8 -translate-y-1/2 group-hover:translate-x-[3.5rem] transition-transform"
+      />
+      <div className="absolute top-1/2 left-0 w-14 -translate-y-1/2 -translate-x-[3.5rem] group-hover:translate-x-[0] transition-transform">
+        <div className="relative">
+          <Image src={frameCTA} alt="frameCTA" className="" />
+          <div className="absolute inset-0 flex flex-col justify-evenly py-6 items-center">
+            <LinkNewTab href="https://t.me/Evermoon_Community">
+              <Image className="w-8" src={Telegram} alt="Telegram" />
+            </LinkNewTab>
+            <LinkNewTab href="https://discord.com/invite/VgtZZwWbuG">
+              <Image className="w-8" src={Discord} alt="Discord" />
+            </LinkNewTab>
+            <LinkNewTab href="https://www.facebook.com/EvermoonOfficialGameFi">
+              <Image className="w-8" src={Facebook} alt="Facebook" />
+            </LinkNewTab>
+            <LinkNewTab href="https://twitter.com/EverMoon_nft">
+              <Image className="w-8" src={Twitter} alt="Twitter" />
+            </LinkNewTab>
+            <LinkNewTab href="https://www.youtube.com/channel/UCFxqPU4dTufi07B13w_mMuw/videos">
+              <Image className="w-8" src={Youtube} alt="Youtube" />
+            </LinkNewTab>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
