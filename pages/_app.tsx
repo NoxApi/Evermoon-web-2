41d63fedfeb27cb6b8c18450a6e0d957470f6097
NoxApi <pcsnon@gmail.com>
2022-10-03 13:46:05 +0700

    test loading 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,8 +21,8 @@ function show() {
  );
   return(
   <GlobalProvider>
-    {isloading&&<div className='w-full h-full flex justify-center items-center absolute inset-0 z-30'>
-        <a className='text-6xl heroes-text-gold bg-bgs6'>LOADING</a>
+    {isloading&&<div className='w-full h-full flex justify-center items-center absolute inset-0 z-30 bg-bgs6'>
+        <a className='text-6xl heroes-text-gold '>LOADING</a>
         </div>}
     <Component {...pageProps} />
   </GlobalProvider>   
