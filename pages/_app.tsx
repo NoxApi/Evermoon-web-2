2ba4d43a4ec6e0c5b1dc2dd33d5ee1db4b6a5d38
NoxApi <pcsnon@gmail.com>
2022-10-03 13:19:17 +0700

    test loading 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,20 @@ import { GlobalProvider } from '../state'
 
 function MyApp({ Component, pageProps }: AppProps) {
  const [isloading,setisloading] = useState(true)
- useEffect(()=>{
+function show() {
   setisloading(false);
- });
+}
+ useEffect(()=>{
+  if (document.readyState === "complete") {
+    
+  } else {
+    window.addEventListener("load", show);
+    // Remove the event listener when component unmounts
+    return () => window.removeEventListener("load", show);
+  }
+}
+ 
+ );
   return(
   <GlobalProvider>
     {isloading&&<div className='w-full h-full flex justify-center items-center absolute inset-0 z-30'>
